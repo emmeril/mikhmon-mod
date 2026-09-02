239571fd3dbc990734b3812c6ebd78e6163e28d0
emmeril <ithelpdesk.smtt@gmail.com>
2026-09-02 14:23:29 +0700

    fix: update dashboard traffic tooltip

--- dashboard/home.php
+++ dashboard/home.php
@@ -320,9 +320,21 @@ if (!isset($_SESSION["mikhmon"])) {
                   
                   <script type="text/javascript"> 
                     var chart;
-                    var sessiondata = "<?= $session ?>";
-                    var interface = "<?= $interface ?>";
+                    var sessiondata = <?= json_encode((string) $session); ?>;
+                    var interface = <?= json_encode((string) $interface); ?>;
+                    var trafficTooltipTitle = <?= json_encode(htmlspecialchars($brandname . ' - ' . $_traffic, ENT_QUOTES, 'UTF-8')); ?>;
+                    var trafficTimeLabel = <?= json_encode(htmlspecialchars($_time, ENT_QUOTES, 'UTF-8')); ?>;
                     var n = 3000;
+
+                    function formatDashboardTrafficRate(value) {
+                      var rate = Number(value) || 0;
+                      var sizes = ['bps', 'kbps', 'Mbps', 'Gbps', 'Tbps'];
+                      if (rate <= 0) return '0 bps';
+
+                      var index = Math.min(Math.floor(Math.log(rate) / Math.log(1024)), sizes.length - 1);
+                      return parseFloat((rate / Math.pow(1024, index)).toFixed(2)) + ' ' + sizes[index];
+                    }
+
                     function requestDatta(session,iface) {
                       $.ajax({
                         url: './traffic/traffic.php?session='+session+'&iface='+iface,
@@ -390,24 +402,20 @@ if (!isset($_SESSION["mikhmon"])) {
                               text: null
                             },
                             labels: {
-                              formatter: function () {      
-                                var bytes = this.value;                          
-                                var sizes = ['bps', 'kbps', 'Mbps', 'Gbps', 'Tbps'];
-                                if (bytes == 0) return '0 bps';
-                                var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-                                return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];                    
+                              formatter: function () {
+                                return formatDashboardTrafficRate(this.value);
                               },
                             },       
                         },
                         
                         series: [{
-                          name: 'Tx',
+                          name: 'TX',
                           data: [],
                           marker: {
                             symbol: 'circle'
                           }
                         }, {
-                          name: 'Rx',
+                          name: 'RX',
                           data: [],
                           marker: {
                             symbol: 'circle'
@@ -415,8 +423,20 @@ if (!isset($_SESSION["mikhmon"])) {
                         }],
 
                         tooltip: {
-                          formatter: function () { 
-                            var _0x2f7f=["\x70\x6F\x69\x6E\x74\x73","\x79","\x62\x70\x73","\x6B\x62\x70\x73","\x4D\x62\x70\x73","\x47\x62\x70\x73","\x54\x62\x70\x73","\x3C\x73\x70\x61\x6E\x20\x73\x74\x79\x6C\x65\x3D\x22\x63\x6F\x6C\x6F\x72\x3A","\x63\x6F\x6C\x6F\x72","\x73\x65\x72\x69\x65\x73","\x3B\x20\x66\x6F\x6E\x74\x2D\x73\x69\x7A\x65\x3A\x20\x31\x2E\x35\x65\x6D\x3B\x22\x3E","\x73\x79\x6D\x62\x6F\x6C\x55\x6E\x69\x63\x6F\x64\x65","\x3C\x2F\x73\x70\x61\x6E\x3E\x3C\x62\x3E","\x6E\x61\x6D\x65","\x3A\x3C\x2F\x62\x3E\x20\x30\x20\x62\x70\x73","\x70\x75\x73\x68","\x6C\x6F\x67","\x66\x6C\x6F\x6F\x72","\x3A\x3C\x2F\x62\x3E\x20","\x74\x6F\x46\x69\x78\x65\x64","\x70\x6F\x77","\x20","\x65\x61\x63\x68","\x3C\x62\x3E\x4D\x69\x6B\x68\x6D\x6F\x6E\x20\x54\x72\x61\x66\x66\x69\x63\x20\x4D\x6F\x6E\x69\x74\x6F\x72\x3C\x2F\x62\x3E\x3C\x62\x72\x20\x2F\x3E\x3C\x62\x3E\x54\x69\x6D\x65\x3A\x20\x3C\x2F\x62\x3E","\x25\x48\x3A\x25\x4D\x3A\x25\x53","\x78","\x64\x61\x74\x65\x46\x6F\x72\x6D\x61\x74","\x3C\x62\x72\x20\x2F\x3E","\x20\x3C\x62\x72\x2F\x3E\x20","\x6A\x6F\x69\x6E"];var s=[];$[_0x2f7f[22]](this[_0x2f7f[0]],function(_0x3735x2,_0x3735x3){var _0x3735x4=_0x3735x3[_0x2f7f[1]];var _0x3735x5=[_0x2f7f[2],_0x2f7f[3],_0x2f7f[4],_0x2f7f[5],_0x2f7f[6]];if(_0x3735x4== 0){s[_0x2f7f[15]](_0x2f7f[7]+ this[_0x2f7f[9]][_0x2f7f[8]]+ _0x2f7f[10]+ this[_0x2f7f[9]][_0x2f7f[11]]+ _0x2f7f[12]+ this[_0x2f7f[9]][_0x2f7f[13]]+ _0x2f7f[14])};var _0x3735x2=parseInt(Math[_0x2f7f[17]](Math[_0x2f7f[16]](_0x3735x4)/ Math[_0x2f7f[16]](1024)));s[_0x2f7f[15]](_0x2f7f[7]+ this[_0x2f7f[9]][_0x2f7f[8]]+ _0x2f7f[10]+ this[_0x2f7f[9]][_0x2f7f[11]]+ _0x2f7f[12]+ this[_0x2f7f[9]][_0x2f7f[13]]+ _0x2f7f[18]+ parseFloat((_0x3735x4/ Math[_0x2f7f[20]](1024,_0x3735x2))[_0x2f7f[19]](2))+ _0x2f7f[21]+ _0x3735x5[_0x3735x2])});return _0x2f7f[23]+ Highcharts[_0x2f7f[26]](_0x2f7f[24], new Date(this[_0x2f7f[25]]))+ _0x2f7f[27]+ s[_0x2f7f[29]](_0x2f7f[28])
+                          formatter: function () {
+                            var lines = [
+                              '<b>' + trafficTooltipTitle + '</b>',
+                              '<b>' + trafficTimeLabel + ':</b> ' + Highcharts.dateFormat('%H:%M:%S', new Date(this.x))
+                            ];
+
+                            $.each(this.points, function (_, point) {
+                              lines.push(
+                                '<span style="color:' + point.series.color + ';font-size:1.5em;">●</span>' +
+                                '<b>' + point.series.name + ':</b> ' + formatDashboardTrafficRate(point.y)
+                              );
+                            });
+
+                            return lines.join('<br>');
                           },
                           shared: true                                                      
                         },
